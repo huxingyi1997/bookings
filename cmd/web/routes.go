da1ca86d33d03111a9e66b4f79a4fa7979331d81
xingyi.hu <xingyi.hu@shopee.com>
2022-07-19 20:07:10 +0800

    Initial commit

---
+++ cmd/web/routes.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/huxingyi1997/bookings/pkg/config"
+	"github.com/huxingyi1997/bookings/pkg/handlers"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func routes(app *config.AppConfig) http.Handler {
+	// mux := pat.New()
+
+	// mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
+	// mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+	mux := chi.NewRouter()
+
+	mux.Use(middleware.Recoverer)
+	mux.Use(NoSurf)
+	mux.Use(Sessionload)
+
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
+	return mux
+}
